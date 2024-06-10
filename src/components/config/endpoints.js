caea27d188f8f34da123cc70962811d61aaf6a0c
Catalina Maria <catalinamariadev@yahoo.com>
2024-06-10 21:01:57 +0300

    change end poind to production

--- src/components/config/endpoints.js
+++ src/components/config/endpoints.js
@@ -1,3 +1,3 @@
-// const API_BASE_URL = 'https://fabricadecoduri-bf3f0e9b8b05.herokuapp.com/';
-const API_BASE_URL = 'http://localhost:8080/';
+const API_BASE_URL = 'https://fabricadecoduri-bf3f0e9b8b05.herokuapp.com/';
+// const API_BASE_URL = 'http://localhost:8080/';
 export { API_BASE_URL };
