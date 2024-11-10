44653d8de1b4b7e9ac22cccaffb5a68994211414
LazariaInk <lorienlored@gmail.com>
2024-11-10 20:23:14 +0300

    delete ocmment

--- api/server.js
+++ api/server.js
@@ -2,13 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Servește fișierele statice din `build` și `public`
 app.use(express.static(path.join(__dirname, 'build')));
 app.use('/lessons', express.static(path.join(__dirname, 'public', 'lessons')));
 app.use('/static', express.static(path.join(__dirname, 'build', 'static')));
 app.use('/manifest.json', express.static(path.join(__dirname, 'build', 'manifest.json')));
 
-// Orice altă cerere este redirecționată către `index.html` pentru suport React Router
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
