4d700bf03c420387b5869e7f5ab8f49cc3db1352
LazariaInk <lorienlored@gmail.com>
2024-11-10 20:30:03 +0300

    modify server.jks

--- api/server.js
+++ api/server.js
@@ -5,10 +5,16 @@ const app = express();
 // Servește fișierele statice din `build`
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Servește fișierele statice din `public/lessons`
+// Servește fișierele statice din `public/lessons` pentru lecții specifice
 app.use('/lessons', express.static(path.join(__dirname, 'public', 'lessons')));
 
-// Redirecționează toate celelalte cereri către `index.html` pentru suport React Router
+// Servire fișiere statice: `/static`, `/manifest.json`, și alte rute specifice
+app.use('/static', express.static(path.join(__dirname, 'build', 'static')));
+app.use('/manifest.json', express.static(path.join(__dirname, 'build', 'manifest.json')));
+app.use('/favicon.ico', express.static(path.join(__dirname, 'build', 'favicon.ico')));
+
+// Orice altă cerere care nu începe cu `/static`, `/lessons`, `/manifest.json`, sau `/favicon.ico` 
+// este redirecționată către `index.html` pentru suport React Router
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
