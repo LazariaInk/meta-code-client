db7e23a5e75463abaa7703575d15cf679ecd454d
LazariaInk <lorienlored@gmail.com>
2024-11-10 20:27:42 +0300

    test

--- api/server.js
+++ api/server.js
@@ -2,15 +2,18 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Servește fișierele statice din `build`
 app.use(express.static(path.join(__dirname, 'build')));
-app.use('/lessons', express.static(path.join(__dirname, 'public', 'lessons')));
-app.use('/static', express.static(path.join(__dirname, 'build', 'static')));
-app.use('/manifest.json', express.static(path.join(__dirname, 'build', 'manifest.json')));
 
+// Servește fișierele statice din `public/lessons`
+app.use('/lessons', express.static(path.join(__dirname, 'public', 'lessons')));
+
+// Redirecționează toate celelalte cereri către `index.html` pentru suport React Router
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+// Setează portul
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
