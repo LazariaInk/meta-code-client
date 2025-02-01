dcaa9c157e0bbf68aba380dbad0fb6c84c01f316
LazariaInk <lorienlored@gmail.com>
2025-02-02 01:39:40 +0300

    add paypall donation

--- src/components/DonationPopup.js
+++ src/components/DonationPopup.js
@@ -17,12 +17,16 @@ function DonationPopup({ onClose }) {
           indiferent de nivelul de cunoștințe sau locație.
         </p>
         <p>
-          Susține viitorul educației digitale acum! Cont Revolut:
-          <strong> RO21 BREL 0005 5453 8388 0100</strong>
-        </p>
-        <p>
           În semn de recunoștință, toți sponsorii vor fi adăugați în <strong>tabela noastră de susținători</strong>, pentru
           a le mulțumi public pentru contribuțiile lor. Fii parte din comunitatea noastră de sprijin!
+        </p>
+        <p>
+          <form action="https://www.paypal.com/donate" method="post" target="_top">
+            <input type="hidden" name="hosted_button_id" value="2R5NB6PAWHUT4" />
+            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+            <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+          </form>
+
         </p>
 
       </div>
